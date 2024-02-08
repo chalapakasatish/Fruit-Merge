0b52fc55781c3df84d98d36c40b9a7316a5990ba
chalapakasatish <chalapakasatish@gmail.com>
2024-02-09 00:08:26 +0530

    08-02-2024 Night

---
+++ Assets/Scripts/FruitManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitManager : MonoBehaviour
+{
+    [Header("Elements")]
+    [SerializeField] private GameObject fruitPrefab;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ManagePlayerInput();
+        }
+    }
+    private void ManagePlayerInput()
+    {
+        Instantiate(fruitPrefab,GetClickedWorldPosition(),Quaternion.identity);
+    }
+    private Vector2 GetClickedWorldPosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
